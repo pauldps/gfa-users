2cd4eaeaeb00976d47f081218d7ac26ce6a522af
pauloddr <github@pauloddr.com>
2017-05-07 14:16:39 -0300

    cors

--- support/test-app.js
+++ support/test-app.js
@@ -12,25 +12,37 @@ expressApp.all('/all', function (req, res) {
   GoogleFunctionAuth.all(req, res);
 });
 
-expressApp.post('/create', function (req, res) {
+expressApp.all('/create', function (req, res) {
   GoogleFunctionAuth.create(req, res);
 });
 
-expressApp.post('/signin', function (req, res) {
+expressApp.all('/update', function (req, res) {
+  GoogleFunctionAuth.update(req, res);
+});
+
+expressApp.all('/find', function (req, res) {
+  GoogleFunctionAuth.find(req, res);
+});
+
+expressApp.all('/list', function (req, res) {
+  GoogleFunctionAuth.list(req, res);
+});
+
+expressApp.all('/signin', function (req, res) {
   GoogleFunctionAuth.signin(req, res);
 });
 
-expressApp.get('/ping', function (req, res) {
+expressApp.all('/ping', function (req, res) {
   GoogleFunctionAuth.ping(req, res);
 });
 
-expressApp.get('/authorized-function', function (req, res) {
+expressApp.all('/authorized-function', function (req, res) {
   GoogleFunctionAuth.authorize(req, res, (req, res, user) => {
     res.status(200).json(user);
   });
 });
 
-expressApp.get('/authorized-function-with-error', function (req, res) {
+expressApp.all('/authorized-function-with-error', function (req, res) {
   GoogleFunctionAuth.authorize(req, res, (req, res, user) => {
     res.status(200).json(user);
   }, (req, res, error) => {
@@ -41,11 +53,11 @@ expressApp.get('/authorized-function-with-error', function (req, res) {
   });
 });
 
-expressApp.get('/authorized-function-without-next', function (req, res) {
+expressApp.all('/authorized-function-without-next', function (req, res) {
   GoogleFunctionAuth.authorize(req, res);
 });
 
-expressApp.post('/signout', function (req, res) {
+expressApp.all('/signout', function (req, res) {
   GoogleFunctionAuth.signout(req, res);
 });
 
