1e6528adc63d25a132ac9af6ae4e8cb54917888d
pauloddr <github@pauloddr.com>
2017-05-14 14:13:39 -0300

    handle trailing slashes in path

--- test/middleware.js
+++ test/middleware.js
@@ -12,10 +12,30 @@ describe('middleware', function () {
     });
   });
 
+  it('returns 404 on PUT without ID and trailing slashes', function (done) {
+    app.put('/users/').end(function (err, res) {
+      expect(res.body.code).to.equal('NOT_FOUND');
+      app.put('/users///').end(function (err, res) {
+        expect(res.body.code).to.equal('NOT_FOUND');
+        done();
+      });
+    });
+  });
+
   it('returns 404 on DELETE without ID', function (done) {
     app.del('/users').end(function (err, res) {
       expect(res.body.code).to.equal('NOT_FOUND');
       done();
+    });
+  });
+
+  it('returns 404 on DELETE without ID and trailing slashes', function (done) {
+    app.del('/users/').end(function (err, res) {
+      expect(res.body.code).to.equal('NOT_FOUND');
+      app.del('/users///').end(function (err, res) {
+        expect(res.body.code).to.equal('NOT_FOUND');
+        done();
+      });
     });
   });
 
